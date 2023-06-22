125e27559d6ae02f05a365b73a116eca45274349
Fernando Campos <ferchoeth@gmail.com>
2023-06-22 14:57:30 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import { UserDataProvider } from './UserDataContext'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'CVPal',
+  title: 'Quick Curriculum',
   description: 'Genera un curriculum en segundos con ChatGPT',
 }
 
