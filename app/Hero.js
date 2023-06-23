788d2f66b31a6eaaf4ece1a74e9430148bdc6e62
ferchoeth <ferchoeth@gmail.com>
2023-06-22 18:53:30 -0600

    refine home and form styles

--- app/Hero.js
+++ app/Hero.js
@@ -1,8 +1,14 @@
 export default function Hero() {
   return (
-    <div className='app mx-auto flex flex-col items-center justify-center min-h-screen bg-purple-50 text-neutral-900 pt-8 pb-11'>
-      <h1 className='text-3xl md:text-5xl font-bold mb-2'>Quick Curriculum</h1>
-      <p className='mb-5 text-lg'>Genera un CV en segundos con ChatGPT</p>
+    <div className='app mx-auto flex flex-col items-center justify-center min-h-screen bg-slate-900 text-neutral-900 pt-8 pb-11'>
+      <h1 className='text-4xl md:text-6xl font-bold mb-2'>
+        <span className='bg-gradient-to-r from-orange-400 to-purple-500 bg-clip-text text-transparent'>
+          Quick Curriculum
+        </span>
+      </h1>
+      <p className='mb-5 text-lg md:text-2xl text-center bg-gradient-to-r from-purple-400 to-orange-400 bg-clip-text text-transparent'>
+        Genera un CV en segundos con ChatGPT
+      </p>
     </div>
   )
 }
