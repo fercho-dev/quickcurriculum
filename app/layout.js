4142b71251c1ceb378cd8a771abb991e92920f15
ferchoeth <ferchoeth@gmail.com>
2023-10-02 18:23:24 -0600

    fix analytics

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { UserDataProvider } from './UserDataContext'
-import Script from 'next/script'
+import Analytics from './Analytics'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,17 +13,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTAG_ID}`}></Script>
-      <Script id="google-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)};
-          gtag('js', new Date());
-
-          gtag('config', ${process.env.GTAG_ID});
-        `}
-      </Script>
       <body id="root" className={inter.className}>
+        <Analytics />
         <UserDataProvider>
           {children}
         </UserDataProvider>
