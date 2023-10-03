0cb8f719757745749a7a2223e060c28b2bc93d31
ferchoeth <ferchoeth@gmail.com>
2023-10-02 18:02:54 -0600

    add google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { UserDataProvider } from './UserDataContext'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,6 +13,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTAG_ID}`}></Script>
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)};
+          gtag('js', new Date());
+
+          gtag('config', ${process.env.GTAG_ID});
+        `}
+      </Script>
       <body id="root" className={inter.className}>
         <UserDataProvider>
           {children}
